a4285e3998730385bb33832fe541f7711d5585ec
DESKTOP-FQ466S0\aktan <zhenyuan992@hotmail.com>
2017-09-02 21:31:02 +0800

    Add form pic

--- public_html/try2/js/try2js.js
+++ public_html/try2/js/try2js.js
@@ -1,45 +1,91 @@
 var myApp = new Framework7();
 var $$ = Dom7;
 var mainView = myApp.addView('.view-main', {dynamicNavbar: true});
+var storedPersonalData;
+var orderDetails;
+var picData;
 myApp.onPageInit('about', function (page) {
     $$('.create-page').on('click', function () {
         createContentPage();
     });
 });
+myApp.onPageInit('form_order', function (page) {
+    //alert("im in new page");
+    $$('.get-order').on('click', function () {
+        orderDetails = myApp.formGetData('form_order');
+        mainView.router.load({url: "try2_form_pic.html"});
+    });
+});
+myApp.onPageInit('form_order_pic', function (page) {
+    
+    //$(function () {
+        $('.image-editor').cropit();
+        $('.export').click(function () {
+            var imageData = $('.image-editor').cropit('export', {
+                originalSize: true,
+                quality: 1
+            }
+            );
+            //$('.hidden-image-data').val(imageData);
+            //$('#result-data').text(imageData);
+            localStorage.setItem("10image", imageData);
+            window.location.href = "try1_page3.html";
+        });
+        $('.rotate-cw').click(function () {
+            $('.image-editor').cropit('rotateCW');
+        });
+        $('.rotate-ccw').click(function () {
+            $('.image-editor').cropit('rotateCCW');
+        });
+    //});
+    alert("im in new page");
+    $$('.get-order').on('click', function () {
+        orderDetails = myApp.formGetData('form_order');
+        mainView.router.load({url: "try2_form_pic.html"});
+    });
+});
 
-myApp.onPageInit('form', function (page) {
+
+
+myApp.onPageInit('form_personal_details', function (page) {
     // Do something here for "about" page
-    if (checkConnection() && CheckBrowser() ) {
+    if (checkConnection() && CheckBrowser()) {
         //continue
         $$('.get-storage-data').on('click', function () {
-            var check_fields = ["name", "email", "phone"];
+            var check_fields = ["name", "email", "phone", "addr1", "postal"];
             var passed_checks = true;
-            for (var i = 0; i < check_fields.length; i++) {
+            for (var i = 0; ((i < check_fields.length) && passed_checks); i++) {
                 var valuee = $$("#" + check_fields[i]).val();
                 if (!valuee) {
                     alert("Please input a value for: " + check_fields[i]);
                     passed_checks = false;
-                }
-                if (i === 1) {
+                } else if (i === 1) {
                     if (!validateEmail(valuee)) {
                         alert("Please input a valid email address");
+                        passed_checks = false;
+                    }
+                } else if (i === 2) {
+                    if (!valuee.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{2,8}$/im)) {
+                        alert("Please input a valid phone number \n (no letters) ");
                         passed_checks = false;
                     }
                 }
             }
             if (passed_checks) {
-                var storedData = myApp.formGetData('my-form');
-                if (storedData) {
-                    alert(JSON.stringify(storedData));
-                    alert(storedData.name);//JSON.stringify(storedData));
+                storedPersonalData = myApp.formGetData('my-form-pd');
+                if (storedPersonalData) {
+                    //alert(JSON.stringify(storedData));
+                    mainView.router.load({url: "try2_form_order.html"});
+                    //alert(storedData.name);//JSON.stringify(storedData));
                 } else {
-                    alert('There is no stored data for this form yet. Try to change any field');
+                    alert('local storage doesnt apply, im going back.');
+                    mainView.router.back(reload = true);
                 }
             }
         });
     } else {
         alert("im going back");
-        mainView.router.back(reload=true);
+        mainView.router.back(reload = true);
     }
 });
 
